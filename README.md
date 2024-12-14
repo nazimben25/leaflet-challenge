cb5b882374b2353186ebe7b833c98f59304c4fb3
nazimben25 <nazimbendjaballah@hotmail.com>
2024-12-14 09:19:02 -0500

    readme : complete the read me file for part 1

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ output for challenge 15 : leaflet
 1) reporsitory organization
 
 
-2) use the reporsitory
+2) use of the reporsitory
 
-to run the code, use html file : leaflet-challenge\Starter_Code\index.html
-to access the code, use JS file : eaflet-challenge\Starter_Code\static\js\logic.js
-
+- to run the code, use html file : leaflet-challenge\Starter_Code\index.html
+- to access the code, use JS file : leaflet-challenge\Starter_Code\static\js\logic.js
+- CSS FILE : static/css/style.css
 
 3) contexte
 choose a dataset to visualize from The USGS (provides earthquake data in a number of different formats, updated every 5 minutes). 
 Visit the USGS GeoJSON [FeedLinks to an external site. page and](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php)
 
     - dataset choosen was last 7days (https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson)
-    - update dec 11th, 2024
+
 
 Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
 
@@ -30,9 +30,25 @@ Include popups that provide additional information about the earthquake when its
 
 Create a legend that will provide context for your map data.
 
+4) data source
+    - API : dataset choosen was last 7days (https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson)
 
+    - map : https://www.openstreetmap.org/copyright
 
-4) external help
+5) libraries
+    - Leaflet CSS
+    - Leaflet JS 
+    - Leaflet MarkerCluster JS
+    - D3 JavaScript
+
+6) external help
 chat-GPt for function 'formatUnixTime' used to convert dates from UNIX
+    => line 107
+
+chat-GPt to build legend
+- add info.legend to css file 
+    => line 12
+
+
 
 
--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -1,7 +1,8 @@
 // Creating the map object
 let myMap = L.map("map", {
-    // define center as town of provo,ut,USA
-    center: [40.14, -111.39],
+    
+    // define center as town of kodiak, alaska, USA
+    center: [57.47, -152.23],
     zoom: 5.3
   });
   
@@ -19,88 +20,95 @@ console.log(url)
   d3.json(url).then(function(response) { 
     console.log(response); 
 
-    // Loop through the features array in the response using a for loop. 
+    // CReate markers : for Loop through the features array in the response
+
     for (let i = 0; i <  response.features.length; i++) {
-                                                        // retrieve coordinates from json file and store them in variables
-                                                        let latitude = response.features[i].geometry.coordinates[0] ;
-                                                        let longitude = response.features[i].geometry.coordinates[1] ; 
+                    // retrieve coordinates from json file and store them in variables
+                    let latitude = response.features[i].geometry.coordinates[0] ;
+                    let longitude = response.features[i].geometry.coordinates[1] ; 
 
-                                                        // retrieve size and depth of earthquake as Magnitude from json file and store it in variables                                                        
-                                                        let mag = response.features[i].properties.mag;
-                                                        let depth = response.features[i].geometry.coordinates[2] ;
+                    // retrieve size and depth of earthquake as Magnitude from json file and store it in variables                                                        
+                    let mag = response.features[i].properties.mag;
+                    let depth = response.features[i].geometry.coordinates[2] ;
 
-                                                        // retrieve other elements to be used in popup from json file and store it in variables    
-                                                        let place = response.features[i].properties.place ;                                                                                                                                              
-                                                        let time =  response.features[i].properties.time ;
-                                                                    // Format the Unix time const using formatUnixTime (see below)
-                                                                    formattedTime = formatUnixTime(time);
+                    // retrieve other elements to be used in popup from json file and store it in variables    
+                    let place = response.features[i].properties.place ;                                                                                                                                              
+ 
+                    let time =  response.features[i].properties.time ;
+                                // Format the Unix time const using formatUnixTime (see below)
+                                formattedTime = formatUnixTime(time);
 
-                                                        // create circle marker  with 
-                                                        // marker's size proportional to the magnitude (x10000 for visibility)
-                                                        // color follows of the depth 
-                                                        L.circle([longitude, latitude], {
-                                                                                        fillOpacity: 0.75,
-                                                                                        color: "white",
-                                                                                        fillColor: colorscale(depth),
-                                                                                        // Setting our circle's radius to equal the output of our markerSize() function:
-                                                                                        // This will make our marker's size proportionate to its population.
-                                                                                        radius: mag*10000
-                                                                                        })
+                    // create circle marker  with 
+                    // marker's size proportional to the magnitude (x10000 for visibility)
+                    // color follows of the depth 
+                    L.circle([longitude, latitude], {
+                                fillOpacity: 0.75,
+                                color: "white",
+                                fillColor: colorscale(depth),
+                                // Setting our circle's radius to equal the output of our markerSize() function:
+                                // This will make our marker's size proportionate to its population.
+                                radius: mag*10000
+                                })
     
     // popups that provide additional information : place, magnitude, depth and date
     .bindPopup(`<h2>Place : ${place}</h2><hr><h3>Magnitude : ${mag}</h3><hr><h3>Depth : ${depth}</h3><hr><h3>Date (mm/dd/yyyy): ${formattedTime}</h3>`)
     .addTo(myMap)  ;
 
-}
+    }
 
     // Add legend to the map
-    const legend = L.control({ position: "bottomright" });
-  
-    legend.onAdd = function(map) {
 
-        const div = L.DomUtil.create("div", "info legend");
+        // create the legend and position it
+        const legend = L.control({ position: "bottomright" });
+    
 
-        const depths = ["<10", "10-30", "30-50", "50-70", "70+"];
-        // const labels = ["<10", "10-30", "30-50", "50-70", "70+"];
-        const colors = ["#008000", "#ADFF2F", "#FAFAD2", "#FFA500", "#FF4500"];
-  
-    // Generate a label with a colored square for each interval 
-    for (let i = 0; i < depths.length; i++) { 
+        legend.onAdd = function(map) {
+
+            const div = L.DomUtil.create("div", "info legend");
+
+            // assign labels and colors in accordingly to the Colorscale function
+
+            const labels = ["<10", "10-30", "30-50", "50-70", "70+"];
+            const colors = ["#008000", "#ADFF2F", "#FAFAD2", "#FFA500", "#FF4500"];
+    
+        // Generate a label with a colored square  
+
+            for (let i = 0; i < labels.length; i++) { 
                     div.innerHTML += 
                     '<i style="background:' + colors[i] + '"></i> ' +
-                     depths[i] + '<br>'
-                } 
-    return div;
-    };
-  
-    legend.addTo(myMap);
-
-
+                    labels[i] + '<br>'
+                        } 
+            return div;
+            };
+        
+        // insert the legend in the map
+        legend.addTo(myMap);
 
 });
 
 // function "colorscale" to assign a color to different depths
+
 function colorscale(depth) {
-                            if (depth > 70) {
-                                return "#FF4500"; // orange red
-                            } else if (depth > 50) {
-                                return "#FFA500"; //orange
-                            } else if (depth > 30) {
-                                return "#FAFAD2"; //LightGoldenRodYellow
-                            } else if (depth > 10) {
-                                return "#ADFF2F"; // Greenyellow
-                            } else {
-                                return "#008000"; // Green
-                            }
-                        }
-
-
+                if (depth > 70) {
+                    return "#FF4500"; // orange red
+                } else if (depth > 50) {
+                    return "#FFA500"; //orange
+                } else if (depth > 30) {
+                    return "#FAFAD2"; //LightGoldenRodYellow
+                } else if (depth > 10) {
+                    return "#ADFF2F"; // Greenyellow
+                } else {
+                    return "#008000"; // Green
+                }
+            }
 
 // Function to convert Unix timestamp to MM-DD-YYYY T format (code From ChatGPT)
+
 function formatUnixTime(unixTime) { 
-const date = new Date(unixTime );  
-const month = ("0" + (date.getMonth() + 1)).slice(-2); // Add leading zero 
-const day = ("0" + date.getDate()).slice(-2); // Add leading zero 
-const year = date.getFullYear(); 
-return `${month}-${day}-${year}`; } 
+                const date = new Date(unixTime );  
+                const month = ("0" + (date.getMonth() + 1)).slice(-2); // Add leading zero 
+                const day = ("0" + date.getDate()).slice(-2); // Add leading zero 
+                const year = date.getFullYear(); 
+            return `${month}-${day}-${year}`; 
+            } 
 
